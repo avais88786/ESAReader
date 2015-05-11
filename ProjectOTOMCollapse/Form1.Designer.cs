b0b49a38daa5684bdff59b98f0804871492fede1
avais88786 <avaisuddin.mohammad@outlook.com>
2015-05-11 21:06:42 +0100

    esareaderV1

---
+++ ProjectOTOMCollapse/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace ProjectOTOMCollapse
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtBoxReference = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(73, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Reference :";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ContextMenuStrip = this.contextMenuStrip1;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.Padding = new System.Windows.Forms.Padding(10);
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.Location = new System.Drawing.Point(66, 110);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(789, 150);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContextMenuStripNeeded += new System.Windows.Forms.DataGridViewCellContextMenuStripNeededEventHandler(this.dataGridView1_CellContextMenuStripNeeded);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.copyToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(103, 26);
+            // 
+            // copyToolStripMenuItem
+            // 
+            this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            this.copyToolStripMenuItem.Size = new System.Drawing.Size(102, 22);
+            this.copyToolStripMenuItem.Text = "Copy";
+            // 
+            // txtBoxReference
+            // 
+            this.txtBoxReference.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxReference.Location = new System.Drawing.Point(203, 52);
+            this.txtBoxReference.Name = "txtBoxReference";
+            this.txtBoxReference.Size = new System.Drawing.Size(206, 29);
+            this.txtBoxReference.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(476, 52);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 29);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(696, 52);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(159, 29);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Reload";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(915, 356);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtBoxReference);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Project OTOMCollapse";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtBoxReference;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem;
+    }
+}
+
---
+++ ProjectOTOMCollapse/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectOTOMCollapse
+{
+    public partial class Form1 : Form
+    {
+        private BindingSource bindingSource1 = new BindingSource();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            dataGridView1.ClipboardCopyMode = DataGridViewClipboardCopyMode.EnableWithoutHeaderText;
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = bindingSource1;
+            string query = File.ReadAllText("query.txt");
+            string reference = txtBoxReference.Text;
+            
+            if (string.IsNullOrEmpty(reference))
+                return;
+
+            GetData(query, reference);
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            var gridColumns = dataGridView1.Columns;
+            var gridRows = dataGridView1.Rows;
+
+            gridColumns.Cast<DataGridViewColumn>().ToList().ForEach(column =>
+            {
+                if (column.HeaderText.Equals("Exchange Type") || column.HeaderText.Equals("Transaction Type"))
+                {
+                    column.DefaultCellStyle.Font = new Font("Arial", 10.5F);
+                }
+
+            });
+
+
+
+            int currentId = 0;
+            int runningRiskId = 0;
+            Color color = GetRandomConsoleColor();
+            foreach (DataGridViewRow row in gridRows)
+            {
+                runningRiskId = (int)row.Cells["RiskId"].Value;
+                string transactionType = row.Cells["Transaction Type"].Value.ToString();
+
+                //if (currentId == 0)
+                //    currentId = (int)row.Cells["RiskId"].Value;
+
+                //if (currentId == runningRiskId)
+                //    row.Cells.Cast<DataGridViewCell>()
+                //        .ToList().ForEach(cel => cel.Style.ForeColor = color);
+                //else
+                //{
+                //    color = GetRandomConsoleColor();
+                if (transactionType.Contains("MTA"))
+                    row.Cells.Cast<DataGridViewCell>()
+                        .ToList().ForEach(cel => cel.Style.ForeColor = Color.Brown);
+                else
+                    row.Cells.Cast<DataGridViewCell>()
+                        .ToList().ForEach(cel => cel.Style.ForeColor = Color.Blue);
+                
+                //    currentId = runningRiskId;
+                //}
+
+                if (row.Cells["Exchange Type"].Value.ToString().Contains("Response"))
+                {
+                    row.Cells["Exchange Type"].Style.ForeColor = Color.Green;
+                    row.Cells["Transaction Type"].Style.ForeColor = Color.Green;
+                    
+
+                }
+
+                if (row.Cells["Exchange Type"].Value.ToString().Contains("Request"))
+                {
+                    row.Cells["Exchange Type"].Style.ForeColor = Color.Red;
+                    row.Cells["Transaction Type"].Style.ForeColor = Color.Red;
+                }
+
+                
+
+
+            }
+            
+
+        }
+
+        private void GetData(string query,string reference)
+        {
+            try
+            {
+                query = query.Replace("REPLACEME", reference);
+
+                string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnectionString"].ConnectionString;
+
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(query, connectionString);
+
+                // Create a command builder to generate SQL update, insert, and 
+                // delete commands based on selectCommand. These are used to 
+                // update the database.
+                SqlCommandBuilder commandBuilder = new SqlCommandBuilder(dataAdapter);
+
+                // Populate a new data table and bind it to the BindingSource.
+                DataTable table = new DataTable();
+                table.Locale = System.Globalization.CultureInfo.InvariantCulture;
+                dataAdapter.Fill(table);
+                bindingSource1.DataSource = table;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error Occured: " + ex.Message);
+            }
+        }
+
+        private void dataGridView1_CellContextMenuStripNeeded(object sender, DataGridViewCellContextMenuStripNeededEventArgs e)
+        {
+            Clipboard.SetText(dataGridView1[e.ColumnIndex, e.RowIndex].Value.ToString());
+        }
+
+
+        private static Random _random = new Random();
+        private static Color GetRandomConsoleColor()
+        {
+
+            KnownColor[] names = (KnownColor[])Enum.GetValues(typeof(KnownColor));
+            KnownColor randomColorName = names[_random.Next(names.Length)];
+            Color randomColor = Color.FromKnownColor(randomColorName);
+            return randomColor;
+        }
+
+    }
+}
